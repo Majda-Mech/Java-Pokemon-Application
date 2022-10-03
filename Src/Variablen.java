f30f78cad98e9625828ebcf08490614c1705e474
Mechda <mechda.zariouh@gmail.com>
2022-10-03 10:54:23 +0200

    adesopdoos

---
+++ Src/Variablen.java
@@ -0,0 +1,17 @@
+package Src;
+
+import javax.xml.namespace.QName;
+import java.util.Arrays;
+import java.util.List;
+
+public class Variablen {
+    type;
+    List<String> attacks = Arrays.asList();
+    name;
+
+    charizard:
+    blastoise;
+    venusaur;
+
+}
+
